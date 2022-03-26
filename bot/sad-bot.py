f94b10f1b55bc479ed7197b4040bbe834ef10923
Junior <72615418+iamjunioru@users.noreply.github.com>
2022-03-26 18:00:50 -0300

    :sad: :robot:

--- bot/sad-bot.py
+++ bot/sad-bot.py
@@ -1,3 +1,4 @@
+# sad bot
 from time import sleep
 from httplib2 import Response
 import tweepy
@@ -20,7 +21,7 @@ def random_phrase():
           'A vida é apenas uma visão momentânea das maravilhas deste assombroso universo, e é triste que tantos se desgastem sonhando com fantasias espirituais.',
           'Mantenha um pouco de coragem no seu coração e use para viver amanhã.',
           'Gosto de fazer as pessoas felizes, mesmo quando eu estou triste.',
-          'algumas horas atrás',
+          ':(',
           'Eu não sei mais qual é o meu estado emocional, sério. Não sei se estou feliz ou triste. Apenas sinto um vazio dentro de mim.',
           "Um amigo de verdade consegue enxergar a tristeza em seus olhos, quando todas as outras pessoas apenas enxergam o seu sorriso.",
           "Ontem depois que você foi embora confesso que fiquei triste como sempre. Mas, pela primeira vez, triste por você.",
@@ -41,16 +42,17 @@ def random_phrase():
           "A tristeza vai durar para sempre. - Vincent Van Gogh",
           "Quando você compara as tristezas da vida real com os prazeres da imaginária, você não vai querer viver de novo, somente sonhar para sempre.",
           "Irônico: Lembranças felizes nos fazem chorar de tristeza.",
-          "As palavras salvaram-me sempre da tristeza",
+          "As palavras salvaram-me sempre da tristeza.",
           "É triste saber que falta alguma coisa e saber que não dá pra comprar, substituir, esquecer, implorar.",
           "Ando tão só. Ando tão triste que às vezes me jogo na cama, meto a cara fundo no travesseiro e quase começo a chorar. Sempre lembro de você nessas horas.",
           "Quando estiver triste, apenas feche os olhos e pense naquilo que te faz sorrir."]
-
-    
+  
     final_phrase = random.choice(p1)
     return final_phrase
 
 while True:
     api.update_status(status=random_phrase().lower())
-    print('o tweet foi enviado com sucesso!')
-    sleep(600) # a cada x min
+    print('------------------------------------------')
+    print('um tweet triste foi enviado com sucesso!')
+    print('------------------------------------------')
+    sleep(500) # a cada x min
